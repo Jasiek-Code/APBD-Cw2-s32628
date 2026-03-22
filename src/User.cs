20d3f9a78d3137c01facda0068504ad2230bd937
Jan Antczak <s32628@pjwstk.edu.pl>
2026-03-22 15:15:51 +0100

    added Student and Emloyee classes

--- src/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace projekt_obiektowy;
-
-public abstract class User(string name, string surname)
-{
-    public Guid Id { get; init; } = Guid.NewGuid();
-    public string Name { get; set; } = name;
-    public string Surname { get; set; } = surname;
-}
---
+++ src/Users.cs
@@ -0,0 +1,31 @@
+﻿namespace projekt_obiektowy;
+
+public abstract class User(string name, string surname)
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Name { get; set; } = name;
+    public string Surname { get; set; } = surname;
+
+    public abstract int MaxRentals { get; }
+    public abstract void Rent();
+}
+
+public class Student(string name, string surname) : User(name, surname)
+{
+    public override int MaxRentals => 2;
+
+    public override void Rent()
+    {
+        
+    }
+}
+
+public class Employee(string name, string surname) : User(name, surname)
+{
+    public override int MaxRentals => 5;
+
+    public override void Rent()
+    {
+        
+    }
+}
