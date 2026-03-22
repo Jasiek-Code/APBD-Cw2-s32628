cee6ab6b1c8599b1fe6ec259e60d7731881dfbfb
Jan Antczak <s32628@pjwstk.edu.pl>
2026-03-22 16:04:31 +0100

    added hardware classes in hardware.cs and interface for renting -  IRentalService

---
+++ src/Hardware.cs
@@ -0,0 +1,27 @@
+﻿namespace projekt_obiektowy;
+
+public abstract class Hardware(string name)
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Name { get; set; } = name;
+
+    public bool IsAvailable { get; set; } = true;
+}
+
+public class Laptop(string name, string processor, int ramGb) : Hardware(name)
+{
+    public string Processor { get; set; } = processor;
+    public int RamGb { get; set; } = ramGb;
+}
+
+public class Projector(string name, int brightness, string resolution) : Hardware(name)
+{
+    public int Brightness { get; set; } = brightness;
+    public string Resolution { get; set; } = resolution;
+}
+
+public class Camera(string name, bool isDigital, int memoryGb) : Hardware(name)
+{
+    public bool IsDigital { get; set; } = isDigital;
+    public int MemoryGb { get; set; } = memoryGb;
+}
---
+++ src/IRentalService.cs
@@ -0,0 +1,7 @@
+﻿namespace projekt_obiektowy;
+
+public interface IRentalService
+{
+    void RentHardware(User user, Hardware hardware);
+    void ReturnHardware(Hardware hardware);
+}
